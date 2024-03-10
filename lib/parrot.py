d5d8643ce96ca406d049784166f142ffc62d0d07
Anna Kioko <annakioko00@gmail.com>
2024-03-10 06:28:50 +0300

    completed assignment

--- lib/parrot.py
+++ lib/parrot.py
@@ -1,2 +1,9 @@
-def parrot():
-    pass
+def parrot(str='Squawk!'):
+    print(str)
+    return str
+
+#define function parrot
+#accept a string argument
+#print and return the string
+#default argument is squawk
+
